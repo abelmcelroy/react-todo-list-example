01c24bfd0823d84d8ece661ea4fead5e025dc4fc
Abel McElroy <abel.h.mcelroy@gmail.com>
2023-04-20 11:40:54 -0400

    server test

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,26 +1,17 @@
-/**
- * Components:
- * - input for creating new list item
- * - list of all list items
- * - list item which includes
- *  - completedness
- *  - button to delete
- *  - label
- * State:
- * - array of list items
- *  - list items will have: labels, completedness
- */
-
+import axios from "axios";
 import React, { useState } from 'react';
 import NewTodoListItemForm from './NewTodoListItemForm.jsx';
 import TodoItemsList from './TodoItemsList.jsx';
 
 export default function App() {
-
   const [todoItems, updateTodoItems] = useState([]);
 
   return (
     <>
+      <button onClick={async () => {
+        const resp = await axios.get('/')
+        console.log(resp)
+      }}>http request</button>
       <NewTodoListItemForm
         currentTodoItems={todoItems}
         updateTodoItems={updateTodoItems}
